081ebb6d07051726a7833bf5872399082e66dfce
Patt Sookmark <pijiya@sookmark.com>
2023-06-23 11:19:37 -0600

    Add parentMode folder,add ts to landing, add btn, add css module

--- src/containers/LandingContainer.tsx
+++ src/containers/LandingContainer.tsx
@@ -1,8 +1,21 @@
+import styles from "./container.module.css";
+import { useNavigate } from "react-router-dom";
+
 const LandingContainer = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/game");
+  };
+  const childName = "Maddie";
   return (
-    <div>
-      <h1>Welcome to the Landing Page</h1>
+    <div className={styles["landing-body"]}>
+      <h1>Hello {childName}, </h1>
+      <h1>Let's learn Thai</h1>
       <p>This is the landing page of your application.</p>
+
+      <button className={styles["play-btn"]} onClick={handleClick}>
+        <i className={`fa-duotone fa-circle-play ${styles["large-icon"]}`}></i>
+      </button>
     </div>
   );
 };
