173933c6dc4ef675c723ab75a11f7504e2744f62
Patt Sookmark <pijiya@sookmark.com>
2023-06-23 14:14:09 -0600

    Add gameBoard components and refactor css

---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,38 @@
+import { WordData } from "../../types/WordData";
+import { WordBtn } from "../WordBTN/wordBTN";
+import styles from "./gameBoard.module.css";
+
+interface GameBoardProps {
+  allWords: WordData[];
+}
+const GameBoard: React.FC<GameBoardProps> = ({ allWords }) => {
+  const playWord = (src: string) => {
+    const audio = new Audio(src);
+    audio.play();
+  };
+
+  const displayWord = (words: WordData[]) => {
+    const mappedData = words.map((el: WordData) => {
+      return (
+        <WordBtn
+          key={el.id}
+          id={el.id}
+          imgSrc={el.img_url}
+          audioSrc={el.audio_url}
+          word={el.word}
+          onClick={playWord}
+        />
+      );
+    });
+    return mappedData;
+  };
+  return (
+    <div>
+      <h1>Welcome to the Game board</h1>
+      <p>This is the board of your application.</p>
+      <div className={styles["word-board"]}>{displayWord(allWords)}</div>
+    </div>
+  );
+};
+
+export default GameBoard;
--- src/containers/GameContainer.tsx
+++ src/containers/GameContainer.tsx
@@ -3,12 +3,12 @@
 import React from "react";
 import { WordBtn } from "../components/WordBTN/wordBTN";
 import { NavBarContainer } from "./NavBarContainer";
-import styles from "../App.module.css";
+import styles from "./container.module.css";
 import { useState, useEffect } from "react";
 
 import { WordData } from "../types/WordData";
 import { WordDataRepository } from "../repository/wordRepo";
-
+import GameBoard from "../components/GameBoard/GameBoard";
 interface GameContainerProps {
   id: number;
   imgSrc: string;
@@ -36,6 +36,7 @@ const GameContainer: React.FC<GameContainerProps> = () => {
     const audio = new Audio(src);
     audio.play();
   };
+
   const displayWord = () => {
     const mappedData = allWords.map((el: WordData) => {
       return (
@@ -52,19 +53,10 @@ const GameContainer: React.FC<GameContainerProps> = () => {
   };
 
   return (
-    <div className={styles.App}>
+    <div className={styles["game-board"]}>
       <NavBarContainer />
       <main className={styles.main}>
-        <h1>
-          <title>Learn Words Game</title>
-          <meta name="description" content="Learning word in new language" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </h1>
-        <div>
-          <p>Hello, Maddie. Let's learn new Thai words</p>
-          <p>play the word by clicking on the images</p>
-        </div>
+        <GameBoard />
         <div className={styles["word-board"]}>{displayWord()}</div>
       </main>
     </div>
