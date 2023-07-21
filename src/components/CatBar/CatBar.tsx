efa46467f9786bcb544206c664481de3ea59dac4
Patt Sookmark <pijiya@sookmark.com>
2023-07-21 13:18:32 -0600

    add mui to catBar component

--- src/components/CatBar/CatBar.tsx
+++ src/components/CatBar/CatBar.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+import Box from "@mui/material/Box";
 
 interface CatBarProps {
   categories: string[];
@@ -14,16 +20,23 @@ export const CatBar: React.FC<CatBarProps> = ({
   };
 
   return (
-    <ul className="nav-bar">
-      {categories.map((categories) => {
-        return (
-          <li key={categories}>
-            <button onClick={() => handleCategorySelection(categories)}>
-              {categories}
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <nav aria-label="category selections bar">
+        {categories.map((categories) => {
+          return (
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  key={categories}
+                  onClick={() => handleCategorySelection(categories)}
+                >
+                  <ListItemText primary={categories} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          );
+        })}
+      </nav>
+    </Box>
   );
 };
