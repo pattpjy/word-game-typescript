e5a47a93a126e458af735020887fa62c875d4f96
Patt Sookmark <pijiya@sookmark.com>
2023-07-21 21:47:57 -0600

    Change MUI component

--- src/components/CatBox/CatBox.tsx
+++ src/components/CatBox/CatBox.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./catBox.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 interface CatBoxContainerProp {
   onCategorySelected: (category: string) => void;
@@ -49,20 +49,28 @@ export const CatBox: React.FC<CatBoxContainerProp> = ({
         })}
       </div>
 
-      <Button
-        variant="text"
+      <IconButton
+        // variant="text"
         size="small"
         aria-label="playCategory"
         onClick={handlePlayClick}
         sx={{
-          width: 200,
-          padding: 1,
-          margin: "2em 0 0 8em",
+          margin: "2em 0 0 0",
+          padding: "0",
           fontSize: "4rem",
+          "&:hover": {
+            background: "none",
+          },
         }}
+        // sx={{
+        //   width: 200,
+        //   padding: 1,
+        //   margin: "2em 0 0 8em",
+        //   fontSize: "4rem",
+        // }}
       >
         <FontAwesomeIcon icon={faCirclePlay} />
-      </Button>
+      </IconButton>
     </div>
   );
 };
