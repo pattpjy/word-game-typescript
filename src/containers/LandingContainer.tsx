61926c81d711f00bf09b939afcc4a7b1989f1cc1
Patt Sookmark <pijiya@sookmark.com>
2023-07-24 10:51:34 -0600

    Change icon button to animation

--- src/containers/LandingContainer.tsx
+++ src/containers/LandingContainer.tsx
@@ -2,10 +2,13 @@ import styles from "./container.module.css";
 import { useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 interface LandingContainerProp {
   childName: string;
 }
+library.add(faCirclePlay);
 
 const LandingContainer: React.FC<LandingContainerProp> = ({ childName }) => {
   const navigate = useNavigate();
@@ -26,7 +29,11 @@ const LandingContainer: React.FC<LandingContainerProp> = ({ childName }) => {
       <Typography variant="h1">Hello {childName}, Let's learn Thai</Typography>
       <br />
       <button className={styles["play-btn"]} onClick={handleClick}>
-        <i className={`fa-duotone fa-circle-play ${styles["large-icon"]}`}></i>
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          beatFade
+          className={styles["large-icon"]}
+        />
       </button>
     </Container>
   );
